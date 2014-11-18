5b12367e2f9ad9726c3cb175593f01e3d455c8fa
galicians <galicians@gmail.com>
2014-11-18 16:04:05 +0000

    event linked to scope

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,4 +2,16 @@
 
 eventsApp.controller('EventController', function EventController($scope) {
 
-})
+  $scope.event = {
+      name: 'Angular Boot Camp',
+      date: '1/1/2013',
+      time: '10:30 am',
+      imageurl: '/img/angularJS.png',
+      location: {
+        address: 'MakersAcademy Headquartes',
+        city: 'Montain View',
+        province: 'CA'
+      },
+    }
+  }
+  );
